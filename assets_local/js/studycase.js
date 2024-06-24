95bd5fc2725e2e919f6e590634543a78b5a5e5fd
matteobregola <matteobregola@gmail.com>
2024-06-24 11:47:07 +0200

    studycase draft

---
+++ assets_local/js/studycase.js
@@ -0,0 +1,18 @@
+// Data for the radar 
+const data = [{
+    label: 'Caldera',
+    data: [46, 90, 69, 28, 31, 71, 40, 41],
+    fill: true,
+    backgroundColor: 'rgba(255, 99, 132, 0.2)',
+    borderColor: 'rgb(255, 99, 132)',
+    pointBackgroundColor: 'rgb(255, 99, 132)',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: 'rgb(255, 99, 132)'
+}];
+
+// function to call to return the data
+function getData() {
+    return data;
+}
+
