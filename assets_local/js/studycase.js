c213ae71b1cbbc7b12bcd9b49ca8299d4903b2dd
matteobregola <matteobregola@gmail.com>
2024-07-08 16:48:44 +0200

    studycase and search indexing

--- assets_local/js/studycase.js
+++ assets_local/js/studycase.js
@@ -1,18 +1,56 @@
 // Data for the radar 
-const data = [{
+const radar_data = [{
     label: 'Caldera',
     data: [46, 90, 69, 28, 31, 71, 40, 41],
     fill: true,
-    backgroundColor: 'rgba(255, 99, 132, 0.2)',
-    borderColor: 'rgb(255, 99, 132)',
-    pointBackgroundColor: 'rgb(255, 99, 132)',
+    backgroundColor: 'rgba(96, 294, 219, 0.2)',
+    borderColor: '#60CCD2',
+    pointBackgroundColor: '#60CCD2',
     pointBorderColor: '#fff',
     pointHoverBackgroundColor: '#fff',
-    pointHoverBorderColor: 'rgb(255, 99, 132)'
+    pointHoverBorderColor: '#60CCD2'
+}];
+
+// Data for the bar 
+const bar_data = [{
+    label: 'Caldera',
+    data: [46, 90, 69, 28, 31, 71, 40, 41],
+    fill: true,
+    backgroundColor: [
+        'rgb(245, 231, 130)',
+        'rgb(113, 191, 121)',
+        'rgb(175, 212, 127)',
+        'rgb(237, 104, 104)',
+        'rgb(240, 134, 110)',
+        'rgb(169, 209, 125)',
+        'rgb(250, 205, 122)',
+        'rgb(250, 214, 125)'
+
+    ],
+    borderColor: [
+        'rgb(245, 231, 130)',
+        'rgb(113, 191, 121)',
+        'rgb(175, 212, 127)',
+        'rgb(237, 104, 104)',
+        'rgb(240, 134, 110)',
+        'rgb(169, 209, 125)',
+        'rgb(250, 205, 122)',
+        'rgb(250, 214, 125)'
+
+    ],
+    pointBackgroundColor: '#60CCD2',
+    pointBorderColor: '#fff',
+    pointHoverBackgroundColor: '#fff',
+    pointHoverBorderColor: '#60CCD2'
 }];
 
 // function to call to return the data
-function getData() {
-    return data;
+function get_radar_data() {
+    return radar_data;
+}
+
+// function to call to return the data
+function get_bar_data() {
+    return bar_data;
 }
 
